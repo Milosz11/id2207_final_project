d334a9e34a3ec08274977e45ca5a87b4dab8ef54
Milosz <mdabro@illinois.edu>
2023-10-22 10:17:10 +0200

    Update permission matrix with more mappings

--- src/entity_objects.cpp
+++ src/entity_objects.cpp
@@ -22,14 +22,63 @@ PermissionMatrix::PermissionMatrix() {
         _rolePermissions[AdministrationDepartmentManager] = vector<MenuOption> {
                 MO_LogOut
         };
+        _rolePermissions[FinancialManager] = vector<MenuOption> {
+                MO_LogOut, MO_CreateOrUpdateEvent
+        };
+        _rolePermissions[ProductionManager] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[ServicesManager] = vector<MenuOption> {
+                MO_LogOut
+        };
         _rolePermissions[SeniorCustomerServiceOfficer] = vector<MenuOption> {
                 MO_LogOut, MO_RegisterClient, MO_CreateOrUpdateEvent
         };
         _rolePermissions[CustomerService] = vector<MenuOption> {
-                MO_LogOut, MO_RegisterClient
+                MO_LogOut, MO_RegisterClient, MO_CreateOrUpdateEvent
         };
-        // _rolePermissions[AdministrationDepartmentManager] = vector<MenuOption> {LogOut};
-        // _rolePermissions[AdministrationDepartmentManager] = vector<MenuOption> {LogOut};
+        _rolePermissions[MarketingOfficer] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[MarketingAssistant] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[SeniorHRManager] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[HRAssistant] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Photographer] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[AudioSpecialist] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[GraphicDesigner] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Decorations] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[ComputerRelated] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Chef] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Waiter] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[VicePresident] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Secretary] = vector<MenuOption> {
+                MO_LogOut
+        };
+        _rolePermissions[Accountant] = vector<MenuOption> {
+                MO_LogOut
+        };
 }
 
 const vector<MenuOption> &PermissionMatrix::getPermissions(Role role) {
